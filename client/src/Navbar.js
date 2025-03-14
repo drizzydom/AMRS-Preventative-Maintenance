d8a64d6b216d2be32ede220dc84346c5b6aa7515
drizzydom <schoolace@gmail.com>
2025-03-14 15:43:42 -0400

    Enhance UI with Bootstrap and Font Awesome; improve card layout and responsiveness

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -6,7 +6,10 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">Maintenance Tracker</Link>
-      <div className="collapse navbar-collapse">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/">Home</Link>
