800c5c2f28f6b5087096bc00dda95dcbaef3b075
drizzydom <schoolace@gmail.com>
2025-03-15 20:15:44 -0400

    Starting Over Again Rip

--- server/App.js
+++
@@ -1,2 +0,0 @@
-// This file should be removed - it's a duplicate of client/src/App.js
-// Delete this file
--- server/index.js
+++
@@ -1,2 +0,0 @@
-// This file should be removed - it's a duplicate of client/src/index.js
-// Delete this file
