526320a946bacb858896ce5a8c01662016281fec
drizzydom <schoolace@gmail.com>
2025-03-18 17:35:26 -0400

    Add email notification setup and update dependencies for Flask-Mail and dotenv while also cleaning up redundant code without sacrificing functionality.

---
+++ email_config_example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Email Server Configuration
+export MAIL_SERVER='smtp.1and1.com'
+export MAIL_PORT=587
+export MAIL_USE_TLS=True
+export MAIL_USERNAME='maintenance@accuratemachinerepair.com'
+export MAIL_PASSWORD='cheese-flower-monkey'
+export MAIL_DEFAULT_SENDER='maintenance@accuratemachinerepair.com'
