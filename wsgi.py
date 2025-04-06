ab1b5e654b89ee0e34e3c9772c99fc23f69391e6
drizzydom <schoolace@gmail.com>
2025-04-06 15:48:56 -0400

    Fixing bleach imports

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,8 @@
-import os
-import sys
-
-# Add the current directory to the path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+"""
+WSGI entry point for the AMRS Preventative Maintenance application.
+This file is used by Gunicorn to serve the application in production.
+"""
 from app import app
 
 if __name__ == "__main__":
-    # Run the app with debug mode according to environment
-    debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run()
