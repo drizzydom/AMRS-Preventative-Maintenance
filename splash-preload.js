c97dc52ae1869ad533cdacd699c1508e74a47985
drizzydom <schoolace@gmail.com>
2025-08-27 21:59:04 -0400

    feat: integrate auto-updater for Electron app; update user display handling in templates; version bump to 1.4.5

--- splash-preload.js
+++ splash-preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
-  onSplashStatus: (callback) => ipcRenderer.on('splash-status', callback)
+  onSplashStatus: (callback) => ipcRenderer.on('splash-status', callback),
+  send: (channel, ...args) => ipcRenderer.send(channel, ...args)
 });
